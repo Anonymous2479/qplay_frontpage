42271beb0bdbb502ce1b2310898dbe2129e871d9
Richard <43411417+Anonymous2479@users.noreply.github.com>
2020-06-10 00:52:06 +0200

    Update VIPTable.vue
    Hestreng Crates

--- components/vip/VIPTable.vue
+++ components/vip/VIPTable.vue
@@ -271,6 +271,14 @@ export default {
               legend: '6 Crates',
             },
             {
+              name: '<span style="color: #ff0000;"><strong>Hestreng Crates</strong></span>',
+              default: false,
+              basic: '1 Crate',
+              medium: '1 Crates',
+              master: '1 Crates',
+              legend: '2 Crates',
+            },
+            {
               name: 'Coins při aktivaci',
               default: false,
               basic: '10 000 Coins',
